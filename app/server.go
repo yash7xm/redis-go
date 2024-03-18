7695c0414ed282bfb0af4c32475c3f3e86606efd
yash7xm <yash.7xm@gmail.com>
2024-03-18 22:58:46 +0530

    respond to multiple pings

--- app/server.go
+++ app/server.go
@@ -4,8 +4,22 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"strings"
 )
+
+const respPong = "+PONG\r\n"
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+
+	if err != nil {
+		panic(err)
+	}
+
+	conn.Write([]byte(respPong))
+}
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
@@ -22,56 +36,6 @@ func main() {
 			os.Exit(1)
 		}
 
-		LogConnection(conn)
-		RespondWith(conn, respPong)
+		go handleConnection(conn)
 	}
-}
-
-func LogConnection(conn net.Conn) {
-	str, err := ReadOutConnection(conn)
-
-	if err != nil {
-		fmt.Printf("Failed to read connection: %v\n", err)
-		return
-	}
-	fmt.Println("Recieved new request:")
-	fmt.Println(str)
-}
-
-const respNoContent = "HTTP/1.1 204 No Content\r\n"
-
-const respPong = "+PONG\r\n"
-
-func RespondWith(conn net.Conn, response string) {
-	defer conn.Close()
-
-	written, err := conn.Write([]byte(response))
-
-	if err != nil {
-		fmt.Printf("Failed to response %v\n", err)
-		return
-	}
-
-	fmt.Printf("Wrote %v response bytes\n", written)
-}
-
-func ReadOutConnection(conn net.Conn) (string, error) {
-	// Initialize buffer size and string builder
-	buffSize := 1024
-	builder := strings.Builder{}
-	bytes := make([]byte, buffSize)
-	amount := buffSize
-
-	// Read data from the connection until there's no more data
-	for amount == buffSize {
-		am, err := conn.Read(bytes)
-		amount = am
-		if err != nil {
-			return "", err
-		}
-		builder.Write(bytes)
-	}
-
-	// Return the complete string and nil error
-	return builder.String(), nil
 }
